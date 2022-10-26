9edfe2bbb260247fdd737bd94cd0141407a37aa8
Bhasuru, Harsha <Harsha.Bhasuru@IRIworldwide.com>
2022-10-26 17:37:39 -0500

    feat(CORS): Cors filter
    Enabled cors globally on port 4200

---
+++ src/main/java/com/infra/gummadibuilt/common/util/WebConfig.java
@@ -0,0 +1,18 @@
+package com.infra.gummadibuilt.common.util;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
+
